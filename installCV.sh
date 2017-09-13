39e08a0682b8b41f850346d1e5741139f151e033
psyman <ktapsyman@gmail.com>
2017-09-13 20:41:50 +0800

    OpenCV installation scripts added. Target version : Ubuntu 16.04 LTS & Python3.5 with latest version of Opencv and Opencv_contrib

---
+++ installCV.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install -y build-essential cmake pkg-config
+apt-get install -y libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt-get install -y libxvidcore-dev libx264-dev
+apt-get install -y libgtk3-dev
+apt-get install -y libatlas-base-dev gfortran
+apt-get install -y git
+git clone https://github.com/opencv/opencv.git
+cd opencv
+mkdir build
+cd build
+git clone https://github.com/opencv/opencv_contrib.git
+pip3 install numpy
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D OPENCV_EXTRA_MODULES_PATH=opencv_contrib/modules \
+    -D PYTHON_EXECUTABLE=/usr/bin/python3 \
+    -D BUILD_EXAMPLES=ON ..
+
+make -j4
+make install
+ldconfig
